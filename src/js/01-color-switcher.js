409a30794f2b0ff2d60fa9314d8c28ad455fb138
Olesya Aldoshyna <olesyaaldoshyna@gmail.com>
2023-01-25 21:16:53 +0200

    done task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,29 @@
+import '../css/common.css';
 
 const refs = {
   body: document.querySelector('body'),
   btnStart: document.querySelector('[data-start]'),
   btnStop: document.querySelector('[data-stop]'),
 };
+
+let timerId = null;
+
+refs.btnStart.addEventListener('click', startChange);
+refs.btnStop.addEventListener('click', stopChange);
+
+function startChange() {
+ 
+  refs.btnStart.setAttribute('disabled', true);
+  timerId = setInterval(
+    () => (refs.body.style.background = getRandomHexColor()),
+    1000
+  );
+}
+
+function stopChange() {
+  clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
